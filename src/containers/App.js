8f0aee6b80451c2508ff73bbdb91346ec1aff55b
dwyfrequency <jdwy215@gmail.com>
2019-06-26 17:50:40 -0400

    reorganized components and containers - cant seem to get routes to work

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,15 @@
-import '../assets/css/App.css'
-import Form from './reuse/Form'
-import React, { Component } from 'react'
+import '../assets/css/App.css';
+import React, { Component } from 'react';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Hello, Electron!</h1>
-        <Form></Form>
         <h1>QLstico!</h1>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
