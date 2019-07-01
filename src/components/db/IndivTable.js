300560d300df7dfe06d84c9e0d67c75a27670c9e
William Golden V <wtgolden5@gmail.com>
2019-07-01 11:29:48 -0400

    feat: populated table with actual db data

--- src/components/db/IndivTable.js
+++ src/components/db/IndivTable.js
@@ -23,85 +23,38 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Frozen yoghurt1", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich1", 237, 9.0, 37, 4.3),
-  createData("Eclair1", 262, 16.0, 24, 6.0),
-  createData("Cupcake2", 305, 3.7, 67, 4.3),
-  createData("Gingerbread3", 356, 16.0, 49, 3.9),
-  createData("Frozen yfoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice creaem sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclafir", 262, 16.0, 24, 6.0),
-  createData("Cuvpcake", 305, 3.7, 67, 4.3),
-  createData("Gingerdbread", 356, 16.0, 49, 3.9),
-  createData("Frozenfs yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice creasfm sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclafsdfir", 262, 16.0, 24, 6.0),
-  createData("Cupcafsdke", 305, 3.7, 67, 4.3),
-  createData("Gingerfdsfvdsbread", 356, 16.0, 49, 3.9),
-  createData("Frozencssd yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cserream sandwich", 237, 9.0, 37, 4.3),
-  createData("Ecseflair", 262, 16.0, 24, 6.0),
-  createData("Cupfewgcake", 305, 3.7, 67, 4.3),
-  createData("Gingeawerbread", 356, 16.0, 49, 3.9)
-];
-
 const IndivTable = () => {
   const { selectedTableData } = useContext(DbRelatedContext);
   const classes = useStyles();
-  console.log(DbRelatedContext);
-  console.log("selected table Data in indivtable component", selectedTableData);
-  console.log("STD.key", selectedTableData[0]);
 
-  // const colHeader = () => {
-  //   for (let i = 0; i < selectedTableData.length; i++) {
-  //     let rowObj = selectedTableData[i];
-  //     if (rowObj) {
-  //       Object.keys(rowObj).map(key => {
-  //         return <TableCell>{key}</TableCell>;
-  //       });
-  //     }
-  //   }
-  // };
-  return (
+  return selectedTableData.length ? (
     <div className={classes.root}>
       <Paper className={classes.paper}>
         <Table className={classes.table} size='small'>
           <TableHead>
             <TableRow>
-              {selectedTableData.length
-                ? Object.keys(selectedTableData[0]).map(key => {
-                    console.log(key);
-                    return <TableCell>{key}</TableCell>;
-                  })
-                : null}
+              {Object.keys(selectedTableData[0]).map(key => {
+                console.log(key);
+                return <TableCell>{key}</TableCell>;
+              })}
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map(row => (
-              <TableRow key={row.name}>
-                <TableCell component='th' scope='row'>
-                  {row.name}
-                </TableCell>
-                <TableCell align='right'>{row.calories}</TableCell>
-                <TableCell align='right'>{row.fat}</TableCell>
-                <TableCell align='right'>{row.carbs}</TableCell>
-                <TableCell align='right'>{row.protein}</TableCell>
+            {selectedTableData.map(row => (
+              <TableRow key={selectedTableData.indexOf(row)}>
+                {Object.values(row).map(value => (
+                  <TableCell key={value.id} component='th' scope='row'>
+                    {value}
+                  </TableCell>
+                ))}
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </Paper>
     </div>
+  ) : (
+    ""
   );
 };
 
