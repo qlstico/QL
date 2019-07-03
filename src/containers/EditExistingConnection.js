eadebbd9cc51eeed867792d093cb3dc39cc668a7
dwyfrequency <jdwy215@gmail.com>
2019-07-03 11:51:46 -0400

    refactoring connection containers and login

--- src/containers/EditExistingConnection.js
+++ src/containers/EditExistingConnection.js
@@ -6,6 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 const { ipcRenderer } = require('electron');
+import { Login } from '../components/index';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -64,51 +65,11 @@ const Edit = props => {
 
   return (
     thisUser && (
-      <div>
-        <h1>Hello, QLstico!</h1>
-        <h1>Redefining databse access starts here!</h1>
-        <form className={classes.container} noValidate onSubmit={handleSubmit}>
-          <TextField
-            label="User"
-            type="text"
-            name="user"
-            className={classes.textField}
-            value={thisUser.user}
-            onChange={handleInputChange}
-            placeholder={thisUser.user}
-          />
-          <TextField
-            label="Password"
-            type="text"
-            name="password"
-            className={classes.textField}
-            value={thisUser.password}
-            onChange={handleInputChange}
-            placeholder={thisUser.password}
-          />
-          <TextField
-            label="Server"
-            type="text"
-            name="server"
-            className={classes.textField}
-            value={thisUser.server}
-            onChange={handleInputChange}
-            placeholder={thisUser.server}
-          />
-          <TextField
-            label="Database Name"
-            type="text"
-            name="databaseName"
-            className={classes.textField}
-            value={thisUser.databaseName}
-            onChange={handleInputChange}
-            placeholder={thisUser.databaseName}
-          />
-          <Button variant="contained" type="submit">
-            Submit
-          </Button>
-        </form>
-      </div>
+      <Login
+        handleSubmit={handleSubmit}
+        handleInputChange={handleInputChange}
+        values={thisUser}
+      />
     )
   );
 };
