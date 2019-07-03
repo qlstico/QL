5f740021ec0ff107c550dca9e88d29d71b1bb6f5
Ricardo Pineda <richay718@gmail.com>
2019-07-03 17:59:43 -0400

    added notes for table grid functionality

--- src/components/db/IndivTable.js
+++ src/components/db/IndivTable.js
@@ -27,36 +27,66 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const IndivTable = () => {
+  // For styling components, referencing this directly in
+  // components className to access the above styling objects ^^^
   const classes = useStyles();
-  const [editRow, setEditRow] = useState(false);
-  const [tableMatrix, setTableMatrix] = useState([]);
+
+  // Grabbing the tabledata from the context provider
   const { selectedTableData } = useContext(DbRelatedContext);
 
+  // Setting the matrix created from the context provider's array of objs
+  // to render the table cells and to have a 'sandbox copy' in the state
+  // to compare changes against the context provider's original version
+  const [tableMatrix, setTableMatrix] = useState([]);
+
+  // Using this as componentDidMount && componentDidUpdate b/c the provider data from
+  // context does not make it in time for the initial mounting
+  useEffect(() => {
+    // Creating a 2-d matrix of the selectedTableData from provider in order to
+    // represent each object in the selectedTableData array as a row, and each of the
+    // values in the 'row obj' as a cell
+    const matrix = selectedTableData.map(row =>
+      // Passing in row id and val as obj to reference inside of
+      // handleInputChange and to reference as an attribute inside component
+      Object.values(row).map(value => ({ value, id: row.id }))
+    );
+    // Setting the matrix created above as the state for the component instead of
+    // just using the context provider directly in order to have a copy we can work with without
+    // affecting the true values represented by the provider. This lets us have
+    // a sandbox to play with any changes without commiting to them until we hit submit.
+    setTableMatrix(matrix);
+    // We're listening for any changes in selectedDataTable since it takes a little
+    // bit for this to come through, therefore we need to update once we actually
+    // get ahold of this to properly set our state an kick off rending of the grid table.
+  }, [selectedTableData]);
+
+  // Handling any changes in the grid's cells - takes the event to identify the target cell, the row
+  // in the matrix the cell exists in, and the ID of the cell's parent row/obj as it exists in the db
   const handleInputChange = (e, matrixRowIdx, dbEntryId) => {
+    // Destructures the 'name' and value of the event target for ease of access to them
     const { name, value } = e.target;
+    // Destructures the rowIdx and colIdx from the string returned by the event.target.name
     const [rowIdx, colIdx] = name.split('-');
 
+    // Makes the changes in state's matrix using the rowIdx and
+    //colIdx to locate it's position and rewritting it's value
     setTableMatrix(prevMatrix => {
+      // Matrix cells contain objects, therefore we need to find
+      //the cell, and reset the value prop within the cell's obj
       prevMatrix[rowIdx][colIdx].value = value;
       return prevMatrix;
     });
   };
 
-  useEffect(() => {
-    // using this as componentDidUpdate b/c the provider data from
-    // context does not make it in time for the initial mounting
-    // const tableDataToUse = tableMatrix.length ? tableMatrix : selectedTableData;
-    const matrix = selectedTableData.map(row =>
-      // Passing in row id and val as obj to reference inside of handleInputChange and to reference as an attribute inside component
-      Object.values(row).map(value => ({ value, id: row.id }))
-    );
-    setTableMatrix(matrix);
-  }, [selectedTableData]);
+  // Tracking which row is in 'edit mode'
+  const [editRow, setEditRow] = useState(false);
 
+  // Sets the selected row for editing
   const enableEditRow = rowIdx => {
     setEditRow(rowIdx);
   };
 
+  // Removes the selected editable row from 'edit mode'
   const removeEditRow = () => {
     setEditRow(false);
   };
@@ -78,10 +108,13 @@ const IndivTable = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {/* Table Data */}
+            {/* Table Rows Data */}
             {tableMatrix.map((row, rowIdx) => (
               <TableRow key={rowIdx}>
+                {/* Rows cell data */}
                 {row.map(({ value, id }, colIdx) =>
+                  // Checks to see if this row is the editable row, if it is render cells as
+                  // textField, else render as a normal read only cells.
                   editRow === rowIdx ? (
                     <TableCell
                       key={`${rowIdx}-${colIdx}`}
@@ -91,6 +124,8 @@ const IndivTable = () => {
                       <TextField
                         type="text"
                         defaultValue={value}
+                        // Name field is how we reference this cell's equivalent
+                        // position in the state matrix to make changes
                         name={`${rowIdx}-${colIdx}`}
                         onChange={e => handleInputChange(e, rowIdx, id)}
                       />
@@ -100,7 +135,11 @@ const IndivTable = () => {
                       key={`${rowIdx}-${colIdx}`}
                       component="th"
                       scope="row"
+                      // Set this row to be the selected row for 'edit mode' in
+                      // the state to rerender as a textField
                       onDoubleClick={() => enableEditRow(rowIdx)}
+                      // If this is not an 'edit mode' row, clicking on it will
+                      // remove 'edit mode'
                       onClick={() => removeEditRow()}
                       name={`${rowIdx}-${colIdx}`}
                     >
