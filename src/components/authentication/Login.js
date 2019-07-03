452c104744ae1491e1bdee5762cf4c777e02a9b7
dwyfrequency <jdwy215@gmail.com>
2019-07-03 17:51:38 -0400

    removed uncessary import login

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import storage from 'electron-json-storage';
+import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import { CommunicationStayPrimaryLandscape } from 'material-ui/svg-icons';
 
 const useStyles = makeStyles(theme => ({
   container: {
