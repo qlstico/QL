bd2752e12278159b0abd0e12ab28d82bdccf709b
dwyfrequency <jdwy215@gmail.com>
2019-07-05 12:26:35 -0400

    initial attempt at updateTableData func

--- src/db/db.js
+++ src/db/db.js
@@ -1,14 +1,14 @@
-const pg = require("pg");
+const pg = require('pg');
 // const storage = require('electron-json-storage');
 // const {app} = require('electron')
 
 const DB_CONNECTION = {
-  user: "", // env var: PGUSER
-  database: "", // env var: PGDATABASE
-  password: "", // env var: PGPASSWORD
-  host: "localhost", // Server hosting the postgres database
+  user: '', // env var: PGUSER
+  database: '', // env var: PGDATABASE
+  password: '', // env var: PGPASSWORD
+  host: 'localhost', // Server hosting the postgres database
   port: 5432, // env var: PGPORT
-  idleTimeoutMillis: 300 // how long a client is allowed to remain idle before being closed
+  idleTimeoutMillis: 300, // how long a client is allowed to remain idle before being closed
 };
 
 const setDatabase = dbName => {
@@ -19,7 +19,7 @@ const getAllDbs = async () => {
   const pool = new pg.Pool(DB_CONNECTION);
   try {
     const response = await pool.query(
-      "SELECT datname FROM pg_database WHERE datistemplate = false"
+      'SELECT datname FROM pg_database WHERE datistemplate = false'
     );
     const arrayOfDbNames = response.rows.map(({ datname }) => {
       return datname;
@@ -58,8 +58,27 @@ const getTableData = async (table, database) => {
   }
 };
 
+const updateTableData = async (table, database, fields, values, dbRowId) => {
+  setDatabase(database);
+  const newArr = [];
+  for (let index = 0; index < fields.length; index++) {
+    newArr.push(`${fields}=${values}`);
+  }
+
+  const pool = new pg.Pool(DB_CONNECTION);
+  try {
+    const response = await pool.query(
+      `UPDATE ${table} SET ${newArr.join(' ')} where id = ${dbRowId}`
+    );
+    console.log(response);
+    return response.rows;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 module.exports = {
   getAllTables,
   getAllDbs,
-  getTableData
+  getTableData,
 };
