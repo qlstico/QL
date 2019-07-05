300506c7140834c6114245dd613903d247abef29
William Golden V <wtgolden5@gmail.com>
2019-07-05 00:16:46 -0400

    feat:created breadcrumbs feature and wired up functionality

--- src/components/reuse/Header.js
+++ src/components/reuse/Header.js
@@ -1,30 +1,31 @@
-import React from 'react';
-import { fade, makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import { withRouter } from 'react-router-dom';
-import { ipcRenderer } from 'electron';
-import storage from 'electron-json-storage';
-import SucioBreadcrumbs from './Breadcrumbs';
-import ArrowBack from '@material-ui/icons/ArrowBack';
-import ArrowForward from '@material-ui/icons/ArrowForward';
+import React from "react";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import { withRouter } from "react-router-dom";
+import { ipcRenderer } from "electron";
+import storage from "electron-json-storage";
+import SucioBreadcrumbs from "./Breadcrumbs";
+import ArrowBack from "@material-ui/icons/ArrowBack";
+import ArrowForward from "@material-ui/icons/ArrowForward";
+import BreadcrumbsElem from "./Breadcrumbs";
 const {
   GET_DB_NAMES,
   GET_DB_NAMES_REPLY,
   CLOSE_SERVER
-} = require('../../constants/ipcNames');
+} = require("../../constants/ipcNames");
 
 const getAllDBNames = async () => {
   await ipcRenderer.send(GET_DB_NAMES);
   await ipcRenderer.on(GET_DB_NAMES_REPLY, (_, databaseNames) => {
-    storage.set('dbnames', databaseNames, error => {
+    storage.set("dbnames", databaseNames, error => {
       if (error) throw error;
     });
   });
@@ -38,26 +39,26 @@ const useStyles = makeStyles(theme => ({
     marginRight: theme.spacing(2)
   },
   title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block'
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block"
     }
   },
   inputRoot: {
-    color: 'inherit'
+    color: "inherit"
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
       width: 200
     }
   },
   sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex'
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex"
     }
   }
 }));
@@ -65,6 +66,7 @@ const useStyles = makeStyles(theme => ({
 function PrimarySearchAppBar(props) {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
+  console.log("PROOOOOPPPSSSS", props);
 
   const isMenuOpen = Boolean(anchorEl);
 
@@ -77,18 +79,18 @@ function PrimarySearchAppBar(props) {
   }
 
   async function sendHome() {
-    props.history.push('/');
+    props.history.push("/");
     await ipcRenderer.send(CLOSE_SERVER);
   }
 
-  const menuId = 'primary-search-account-menu';
+  const menuId = "primary-search-account-menu";
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
       id={menuId}
       keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
@@ -99,40 +101,43 @@ function PrimarySearchAppBar(props) {
 
   return (
     <div className={classes.grow}>
-      <AppBar position="static">
+      <AppBar position='static'>
         <Toolbar>
           <Typography
             className={classes.title}
-            variant="h6"
+            variant='h6'
             noWrap
             onClick={sendHome}
           >
             qlStico
           </Typography>
-          <SucioBreadcrumbs />
+          <BreadcrumbsElem
+            location={props.location.pathname}
+            history={props.history}
+          />
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
             <IconButton
-              aria-label="autorenew"
-              color="inherit"
+              aria-label='autorenew'
+              color='inherit'
               onClick={() => props.history.goBack()}
             >
               <ArrowBack />
             </IconButton>
             <IconButton
-              aria-label="autorenew"
-              color="inherit"
+              aria-label='autorenew'
+              color='inherit'
               onClick={() => props.history.goForward()}
             >
               <ArrowForward />
             </IconButton>
             <IconButton
-              edge="end"
-              aria-label="Account of current user"
+              edge='end'
+              aria-label='Account of current user'
               aria-controls={menuId}
-              aria-haspopup="true"
+              aria-haspopup='true'
               onClick={handleProfileMenuOpen}
-              color="inherit"
+              color='inherit'
             >
               <AccountCircle />
             </IconButton>
