7d656b3ddfcfc3d7dff42cc81e283fcfca8d8b40
Manuel Bugallo <marvinody@gmail.com>
2019-07-08 13:55:27 -0400

    feat: allow passing port to program by PORT=9001

--- main.js
+++ main.js
@@ -121,7 +121,7 @@ function createWindow() {
   if (dev && process.argv.indexOf("--noDevServer") === -1) {
     indexPath = url.format({
       protocol: 'http:',
-      host: 'localhost:8080',
+      host: `localhost:${process.env.PORT || 9000}`,
       pathname: 'index.html',
       slashes: true
     });
